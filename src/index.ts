90e0dd4032cc202d679e8cee0ed454921300bff7
Rinkal-2024 <rinkal.dhankariya@iflair.com>
2024-04-22 11:02:28 +0530

    update

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,11 @@ dotenv.config();
 import express from "express";
 import cors from "cors"
 import jwt from "jsonwebtoken"
-import { sample_foods, sample_tags, sample_users } from "./src/data";
-import foodRouter from './src/router/food.router';
-import userRouter from './src/router/user.router';
-import { dbConnect } from './src/configs/database.config';
-import orderRouter from './src/router/order.router';
+import { sample_foods, sample_tags, sample_users } from "./data";
+import foodRouter from './router/food.router';
+import userRouter from './router/user.router';
+import { dbConnect } from './configs/database.config';
+import orderRouter from './router/order.router';
 dbConnect();
 
 const app = express();
