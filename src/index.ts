ab747acb77f491e56ce8219d30831b279d6298f0
Rinkal-2024 <rinkal.dhankariya@iflair.com>
2024-04-22 14:38:48 +0530

    update

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,8 @@ import cors from "cors"
 const app = express();
 app.use(express.json());
 
+
+
 // app.use((req, res, next) => {
 //     res.setHeader('Access-Control-Allow-Origin', 'https://food-store1-alpha.vercel.app');
 //     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
