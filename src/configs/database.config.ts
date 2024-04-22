2ebbc6f6683719a833a5e0c0f8a10f0481dfb8d3
Rinkal-2024 <rinkal.dhankariya@iflair.com>
2024-04-22 14:20:15 +0530

    update

--- src/configs/database.config.ts
+++ src/configs/database.config.ts
@@ -2,9 +2,9 @@ import {connect ,ConnectOptions } from "mongoose";
 
 
 export const dbConnect = () =>{
-    connect('mongodb+srv://rinkal:6omMIaumiYkiZjyQ@cluster0.k3xiiif.mongodb.net/Food-Store?retryWrites=true&w=majority&appName=Cluster0',{
-        // userNewUrlParser : true,
-        // userUniFiedTopology :true
+    connect('mongodb+srv://rinkal:FkDHnl00inldHlaS@cluster0.k3xiiif.mongodb.net/Food-Store?retryWrites=true&w=majority&appName=Cluster0',{
+        //  userNewUrlParser : true,
+        //  userUniFiedTopology :true
 
     } as ConnectOptions).then(
         () => console.log("connect successfully"),
--- src/index.ts
+++ src/index.ts
@@ -21,15 +21,15 @@ app.use(express.json());
 //     res.setHeader('Access-Control-Allow-Credentials', '');
 //     next();
 //   });
-app.use(cors({
-    credentials : true,
-    origin : ["https://food-store1-alpha.vercel.app"]
-}));
+// app.use(cors({
+//     credentials : true,
+//     origin : ["https://food-store1-alpha.vercel.app"]
+// }));
 
+app.use(cors());
 app.use("/api/food" ,foodRouter);
 app.use("/api/users" ,userRouter);
 app.use("/api/orders" ,orderRouter);
-
 
 app.get('/' , (req,res)=>{
     res.status(200).send('hello')
@@ -39,7 +39,7 @@ app.get('/home', (req,res)=>{
     res.send('hii')
 })
 
-const port = process.env.port || 5000;
+const port = process.env.port || 7000;
 app.listen(port, () => {
     console.log("your website is served on http://localhost:"+port);
 });
