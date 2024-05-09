124f291da8ae01c5c8843a22e9a3a6f13667ef0c
Rinkal-2024 <159881222+Rinkal-2024@users.noreply.github.com>
2024-05-09 11:05:27 +0530

    Update database.config.ts

--- src/configs/database.config.ts
+++ src/configs/database.config.ts
@@ -2,7 +2,7 @@ import {connect ,ConnectOptions } from "mongoose";
 
 
 export const dbConnect = () =>{
-    connect('mongodb+srv://rinkal:FkDHnl00inldHlaS@cluster0.k3xiiif.mongodb.net/Food-Store?retryWrites=true&w=majority&appName=Cluster0',{
+    connect('MONGO_URI!,{
         //  userNewUrlParser : true,
         //  userUniFiedTopology :true
 
